caee65c69dd4177efbf618db7cc8781a9e50269e
John Hernandez <johnhernandez@Johns-MacBook-Air.local>
2016-07-25 15:25:40 -0500

    I've created a table in my codeup test db called albums. Have yet to fill in.

---
+++ albums-migration.sql
@@ -0,0 +1,14 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+create table albums (
+
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	artist VARCHAR(200),
+	name VARCHAR(500),
+	release_date DATE,
+	sales FLOAT,
+	genre VARCHAR(200),
+	primary key(id)
+)
