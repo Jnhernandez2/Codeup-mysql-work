3cbbf9afac283102b38f49e156b6a7b0d2c67589
John Hernandez <johnhernandez@Johns-MacBook-Air.local>
2016-07-26 14:23:09 -0500

    I've updated my seeder file to truncate the table before filling it again, and I've created a new file to update the information in the database.

--- albums-seeder.sql
+++ albums-seeder.sql
@@ -1,4 +1,5 @@
 USE codeup_test_db;
+TRUNCATE * FROM albums;
 
 INSERT INTO albums (artist, name, release_date, sales, genre) VALUES('Pink Floyd', 'Dark Side of The Moon', 1973, 22.7, 'Progressive Rock'), 
 ('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', 1967, 13.1, 'Rock'), ('Led Zeppelin', 'Led Zeppelin IV', 1971, 29.0, 'Hard Rock/Heavy Metal'), ('Fleetwood Mac', 'Rumours', 1977, 27.9, 'Soft Rock');
---
+++ update-exercises.sql
@@ -0,0 +1,12 @@
+use codeup_test_db;
+
+SELECT * FROM albums;
+
+SELECT * FROM albums WHERE release_date < 1980;
+
+SELECT * FROM albums WHERE name = 'Michael Jackson';
+
+UPDATE albums SET sales = sales * 10;
+
+SELECT * FROM albums;
+
