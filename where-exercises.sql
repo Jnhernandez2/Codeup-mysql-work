fbd25e2ef3e3865375eeff513284e5d06c2a0619
John Hernandez <johnhernandez@Johns-MacBook-Air.local>
2016-07-27 13:39:56 -0500

    I've changed the order by order.

--- where-exercises.sql
+++ where-exercises.sql
@@ -1,6 +1,6 @@
 use employees;
 
-SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M' ORDER BY first_name ASC, last_name ASC;
+SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M' ORDER BY last_name ASC, first_name ASC;
 SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
 SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
 SELECT * FROM employees WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-31') AND birth_date LIKE '%-12-25';
