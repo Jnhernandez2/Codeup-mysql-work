5cdf368060851c76ca3462ed024a7a1d38497a3a
John Hernandez <johnhernandez@Johns-MacBook-Air.local>
2016-07-27 15:00:57 -0500

    I've finished off the group by query exercise.

--- group-by-exercise.sql
+++ group-by-exercise.sql
@@ -1,3 +1,6 @@
 USE employees;
 
 SELECT DISTINCT title FROM titles ORDER BY title ASC;
+SELECT last_name FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E' GROUP BY last_name;
+SELECT last_name, first_name FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E' GROUP BY last_name, first_name;
+SELECT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
